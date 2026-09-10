f7447d5833a27663de352967608d101e1d878aee
SHL Organizer <Negaahenorobotic@gmail.com>
2026-09-10 00:08:57 -0700

    launchers rebuilt

--- tools/app.py
+++ tools/app.py
@@ -37,7 +37,7 @@ import zipfile
 
 # The build this app was made from. tools/release stamps it; the site
 # publishes the current one at /downloads/version.json.
-BUILD = '76e8489'
+BUILD = '844f5d6'
 UPDATE_URL = 'https://smarthomeleague.ir/downloads/version.json'
 DOWNLOAD_PAGE = 'https://smarthomeleague.ir/getting-started'
 LOG = os.path.join(tempfile.gettempdir(), 'shl-log.txt')
